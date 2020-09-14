5c2769d3450aee607b71bc0445b3a23d2e46cba9
EncodedWorlds <sam.encode@gmail.com>
2020-09-13 23:31:33 -0700

    Added UE4 Project Files
    Added Unreal Engine 4.25 project files. Includes C++ source, along with blueprint child assets and a default level to test with.

---
+++ Source/Boids/Boids.h
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+//define custom collision channel for boid avoidance tracing
+#define COLLISION_AVOIDANCE				ECC_GameTraceChannel1
---
+++ Source/Boids/BoidsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BoidsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOIDS_API ABoidsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
